e449a6b42f26a448ebc2973a8368a4358c7d9b4f
Angelika Antonenko <luckyhedgehooog@gmail.com>
2023-09-24 21:38:54 +0300

    add change

--- my_cache/src/my_cache.erl
+++ my_cache/src/my_cache.erl
@@ -1,5 +1,5 @@
 -module(my_cache).
--export([create/1, insert/3, insert/4]).
+-export([create/1, insert/3, insert/4, lookup/2]).
 
 create(TableName) ->
   ets:new(TableName, [public, named_table, set]).
@@ -8,7 +8,24 @@ insert(TableName, Key, Value) ->
     ets:insert(TableName, {Key, Value, 0}).
 
 insert(TableName, Key, Value, TimeInSecunds) ->
-    Now = calendar:now_to_datetime(erlang:timestamp()),
+    Now = calendar:universal_time(),
     NewSecunds = calendar:datetime_to_gregorian_seconds(Now)+TimeInSecunds,
     ets:insert(TableName, {Key, Value, NewSecunds}).
+
+lookup(TableName, Key) ->
+    [{Key,Value,TimeInSecunds}] = ets:lookup(TableName,Key),
+    if
+      TimeInSecunds > 0  ->
+        Now = calendar:universal_time(),
+        DifferentSecunds = calendar:datetime_to_gregorian_seconds(Now) - TimeInSecunds,
+        if
+             DifferentSecunds > 0 -> 
+                undefined;
+        true ->
+            Value
+        end;   
+    true ->
+        Value
+    end.  
+
 
